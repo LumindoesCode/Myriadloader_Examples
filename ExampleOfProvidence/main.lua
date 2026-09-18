62941b82f00acf7f3ff4ab5ef49c77655689f7d4
LumindoesCode <93842581+LumindoesCode@users.noreply.github.com>
2026-09-18 09:08:14 +0200

    Update main.lua

--- ExampleOfProvidence/main.lua
+++ ExampleOfProvidence/main.lua
@@ -5,8 +5,8 @@ debug = global_data()
 function debug.Step()
     --This line checks for whenever the R button is pressed, and executes the code inside of it.
     if (call_function("keyboard_check_pressed", {call_function("ord", {"R"})}) == 1) then
-        --Despite the name, spawn_enemy() can be used for any vanilla object too.
-        local damage_up = spawn_enemy(view_x + 120, view_y + 120, "obj_damage_up")
+        --use spawn_object to, well, spawn objects.
+        local damage_up = spawn_object(view_x + 120, view_y + 120, "obj_damage_up")
         --The line below takes the damage_up variable (which has the damage up object),
         --and sets its shop cost to 0.
 
@@ -23,6 +23,7 @@ register_data(debug)
 --REQUIRED FUNCTION: Put your require()s in here
 function mod_load()
     require("ExampleOfProvidence/enemies/movingEnemy")
+    require("ExampleOfProvidence/enemies/shootingEnemy")
 end
 
 --REQUIRED FUNCTION: Currently mostly useless, but required nonetheless
